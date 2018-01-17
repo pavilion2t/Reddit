5362224f125d4aebb63d662ab574d9fe8ca6d982
Judy <850832627@qq.com>
2018-01-17 17:06:31 +0800

    出现跨域问题

--- components/Posts.js
+++ components/Posts.js
@@ -1,16 +1,10 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React from "react"
 
-export default class Posts extends Component {
-  render() {
-    return (
-      <ul>
-        {this.props.posts.map((post, i) => <li key={i}>{post.title}</li>)}
-      </ul>
-    )
-  }
-}
+const Posts = ({posts}) => (
+  <ul>
+    {posts.map((post, i) =>
+      <li key={i}>{post.title}</li>)}
+  </ul>
+)
 
-Posts.propTypes = {
-  posts: PropTypes.array.isRequired
-}
+export default Posts
