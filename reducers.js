5616dcb6c33d9f7bb034b5e2cea5216a0b46389d
Judy <850832627@qq.com>
2018-01-17 17:09:41 +0800

    Update reducers.js

--- reducers.js
+++ reducers.js
@@ -1,12 +1,9 @@
 import { combineReducers } from 'redux'
-import {
-  SELECT_SUBREDDIT,
-  INVALIDATE_SUBREDDIT,
-  REQUEST_POSTS,
-  RECEIVE_POSTS
-} from './actions'
+import { SELECT_SUBREDDIT,
+  REQUEST_POSTS, RECEIVE_POSTS
+} from '../actions'
 
-function selectedSubreddit(state = 'reactjs', action) {
+const selectedSubreddit = (state = 'reactjs', action) => {
   switch (action.type) {
     case SELECT_SUBREDDIT:
       return action.subreddit
@@ -15,44 +12,35 @@ function selectedSubreddit(state = 'reactjs', action) {
   }
 }
 
-function posts(
-  state = {
-    isFetching: false,
-    didInvalidate: false,
-    items: []
-  },
-  action
-) {
+const posts = (state = {isFetching: false,didInvalidate: false,items: []}, action) => {
   switch (action.type) {
-    case INVALIDATE_SUBREDDIT:
-      return Object.assign({}, state, {
-        didInvalidate: true
-      })
     case REQUEST_POSTS:
-      return Object.assign({}, state, {
+      return {
+        ...state,
         isFetching: true,
         didInvalidate: false
-      })
+      }
     case RECEIVE_POSTS:
-      return Object.assign({}, state, {
+      return {
+        ...state,
         isFetching: false,
         didInvalidate: false,
         items: action.posts,
         lastUpdated: action.receivedAt
-      })
+      }
     default:
       return state
   }
 }
 
-function postsBySubreddit(state = {}, action) {
+const postsBySubreddit = (state = { }, action) => {
   switch (action.type) {
-    case INVALIDATE_SUBREDDIT:
     case RECEIVE_POSTS:
     case REQUEST_POSTS:
-      return Object.assign({}, state, {
+      return {
+        ...state,
         [action.subreddit]: posts(state[action.subreddit], action)
-      })
+      }
     default:
       return state
   }
