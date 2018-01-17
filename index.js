0b07324bd484f2ec83042869546b45033a66e8c8
Judy <850832627@qq.com>
2018-01-17 17:10:38 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-import 'babel-polyfill'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './containers/App';
+import { createStore, applyMiddleware } from "redux"
+import { Provider } from "react-redux"
+import reducer from "./reducers"
+import thunk from 'redux-thunk'
 
-import React from 'react'
-import { render } from 'react-dom'
-import Root from './containers/Root'
+const middleware = [ thunk ]
+const store = createStore(reducer, applyMiddleware(...middleware))
 
-render(
-  <Root />,
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
-)
+);
