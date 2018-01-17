fb1f6051df61901b89fe75d81f8278b577ea63b7
Judy <850832627@qq.com>
2018-01-17 17:05:59 +0800

    出现跨域问题

--- components/Picker.js
+++ components/Picker.js
@@ -1,27 +1,15 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React from "react"
 
-export default class Picker extends Component {
-  render() {
-    const { value, onChange, options } = this.props
+const Picker = ({ value, onChange, options }) => (
+  <span>
+    <h1>{value}</h1>
+    <select onChange={e => onChange(e.target.value)} value={value}>
+      {options.map(option =>
+        <option value={option} key={option}>
+          {option}
+        </option>)}
+    </select>
+  </span>
+)
 
-    return (
-      <span>
-        <h1>{value}</h1>
-        <select onChange={e => onChange(e.target.value)} value={value}>
-          {options.map(option => (
-            <option value={option} key={option}>
-              {option}
-            </option>
-          ))}
-        </select>
-      </span>
-    )
-  }
-}
-
-Picker.propTypes = {
-  options: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
-  value: PropTypes.string.isRequired,
-  onChange: PropTypes.func.isRequired
-}
+export default Picker
