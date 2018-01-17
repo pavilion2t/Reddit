16503cc11c69ea547ac34c2ab85400ed7d40c114
Judy <850832627@qq.com>
2018-01-17 17:08:52 +0800

    出现跨域问题

--- actions.js
+++ actions.js
@@ -1,64 +1,40 @@
-import fetch from 'cross-fetch'
-
 export const REQUEST_POSTS = 'REQUEST_POSTS'
 export const RECEIVE_POSTS = 'RECEIVE_POSTS'
 export const SELECT_SUBREDDIT = 'SELECT_SUBREDDIT'
-export const INVALIDATE_SUBREDDIT = 'INVALIDATE_SUBREDDIT'
 
-export function selectSubreddit(subreddit) {
-  return {
-    type: SELECT_SUBREDDIT,
-    subreddit
-  }
+export const selectSubreddit = subreddit => ({
+  type: SELECT_SUBREDDIT,
+  subreddit
+})
+
+export const requestPosts = subreddit => ({
+  type: REQUEST_POSTS,
+  subreddit
+})
+
+export const receivePosts = (subreddit, json) => ({
+  type: RECEIVE_POSTS,
+  subreddit,
+  posts: json.data.children.map(child => child.data),
+  reveivedAt: Date.now()
+})
+
+const fetchPosts = subreddit => dispatch => {
+  dispatch(requestPosts(subreddit))
+  return fetch(`https://wwww.reddit.com/r/${subreddit}.json`)
+    .then(response => response.json())
+    .then(json => dispatch(receivePosts(subreddit, json)))
 }
 
-export function invalidateSubreddit(subreddit) {
-  return {
-    type: INVALIDATE_SUBREDDIT,
-    subreddit
-  }
-}
-
-function requestPosts(subreddit) {
-  return {
-    type: REQUEST_POSTS,
-    subreddit
-  }
-}
-
-function receivePosts(subreddit, json) {
-  return {
-    type: RECEIVE_POSTS,
-    subreddit,
-    posts: json.data.children.map(child => child.data),
-    receivedAt: Date.now()
-  }
-}
-
-function fetchPosts(subreddit) {
-  return dispatch => {
-    dispatch(requestPosts(subreddit))
-    return fetch(`https://www.reddit.com/r/${subreddit}.json`)
-      .then(response => response.json())
-      .then(json => dispatch(receivePosts(subreddit, json)))
-  }
-}
-
-function shouldFetchPosts(state, subreddit) {
+const shouldFetchPosts = (state, subreddit) => {
   const posts = state.postsBySubreddit[subreddit]
-  if (!posts) {
-    return true
-  } else if (posts.isFetching) {
-    return false
-  } else {
-    return posts.didInvalidate
-  }
+  if (!posts) { return true }
+  if (posts.isFetching) { return false }
+  return posts.didInvalidate
 }
 
-export function fetchPostsIfNeeded(subreddit) {
-  return (dispatch, getState) => {
-    if (shouldFetchPosts(getState(), subreddit)) {
-      return dispatch(fetchPosts(subreddit))
-    }
+export const fetchPostsIfNeeded = subreddit => (dispatch, getState) => {
+  if (shouldFetchPosts(getState(), subreddit)) {
+    return dispatch(fetchPosts(subreddit))
   }
 }
