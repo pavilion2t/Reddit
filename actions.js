dafb2b468d83a02b49be05150bafeeb108d023ae
Judy <850832627@qq.com>
2018-01-17 17:58:17 +0800

    Update actions.js

--- actions.js
+++ actions.js
@@ -1,9 +1,15 @@
 export const REQUEST_POSTS = 'REQUEST_POSTS'
 export const RECEIVE_POSTS = 'RECEIVE_POSTS'
 export const SELECT_SUBREDDIT = 'SELECT_SUBREDDIT'
+export const INVALIDATE_SUBREDDIT = 'INVALIDATE_SUBREDDIT'
 
 export const selectSubreddit = subreddit => ({
   type: SELECT_SUBREDDIT,
+  subreddit
+})
+
+export const invalidateSubreddit = subreddit => ({
+  type: INVALIDATE_SUBREDDIT,
   subreddit
 })
 
@@ -16,20 +22,24 @@ export const receivePosts = (subreddit, json) => ({
   type: RECEIVE_POSTS,
   subreddit,
   posts: json.data.children.map(child => child.data),
-  reveivedAt: Date.now()
+  receivedAt: Date.now()
 })
 
 const fetchPosts = subreddit => dispatch => {
   dispatch(requestPosts(subreddit))
-  return fetch(`https://wwww.reddit.com/r/${subreddit}.json`)
+  return fetch(`https://www.reddit.com/r/${subreddit}.json`)
     .then(response => response.json())
     .then(json => dispatch(receivePosts(subreddit, json)))
 }
 
 const shouldFetchPosts = (state, subreddit) => {
   const posts = state.postsBySubreddit[subreddit]
-  if (!posts) { return true }
-  if (posts.isFetching) { return false }
+  if (!posts) {
+    return true
+  }
+  if (posts.isFetching) {
+    return false
+  }
   return posts.didInvalidate
 }
 
